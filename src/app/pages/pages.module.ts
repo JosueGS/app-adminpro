d17f217d77e2d6862e46a74534c4fafced316fe2
Josuegs <jgalarza097@gmail.com>
2021-03-07 00:40:38 -0500

    fin seccion 4 - router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+/* import { AppRoutingModule } from '../app-routing.module';
+SOL 1 cuando no renderiza los componentes, se llama al router
+*/
+import { RouterModule } from '@angular/router';
+
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports:[
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
